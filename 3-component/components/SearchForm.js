c7aeaf6b75c394258477a38127b61fec34502c36
soeun <70000city@gmail.com>
2021-08-23 18:39:18 +0900

    yarn 설치, search form 생성

--- 3-component/components/SearchForm.js
+++ 3-component/components/SearchForm.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 
-class SearchForm extends Component {
+export default class SearchForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { searchForm: "",}
+    this.state = { searchKeyword: "",};
   }
-  
+
+  handleChangeInput(event) {
+    const searchKeyword = event.target.value;
+    this.setState({ searchKeyword });
+  }
+
   render() { 
-    const { searchForm } = this.state;
     return (  
       <form>
         <input
           type="text"
           placeholder="검색어를 입력하세요"
           autoFocus
-          value={searchForm}
+          value={this.state.searchKeyword}
+          onChange={(event) => this.handleChangeInput(event)}
         />
+        {this.state.searchKeyword.length > 0 && (
+          <button type="reset" className="btn-reset"></button>
+        )}
       </form>
     );
   }
 }
-
-export default SearchForm;
